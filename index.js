44fbf37fe34e796c3f3cf9c8b2a5377f23ba6b0a
lotta <46082602+Airile@users.noreply.github.com>
2025-01-29 14:15:07 +0200

    Made dropdown functional
    When city selected from dropdown:
    - gets the timezone of the selected city
    - gets the correct city name (splits into array and picks the name)
    - replaces the html on the page with new html with current city time + date

--- index.js
+++ index.js
@@ -2,24 +2,53 @@
 function updateTime() {
   // LA
   let losAngeles = document.querySelector("#los-angeles");
-  let losAngelesDate = losAngeles.querySelector(".date");
-  let losAngelesTime = losAngeles.querySelector(".time");
-  let losAngelesCurrentTime = moment().tz("America/Los_Angeles");
-
-  losAngelesDate.innerHTML = losAngelesCurrentTime.format("MMMM Do YYYY");
-  losAngelesTime.innerHTML = losAngelesCurrentTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
-  // Sydney
+  if (losAngeles) {
+    let losAngelesDate = losAngeles.querySelector(".date");
+    let losAngelesTime = losAngeles.querySelector(".time");
+    let losAngelesCurrentTime = moment().tz("America/Los_Angeles");
+    losAngelesDate.innerHTML = losAngelesCurrentTime.format("MMMM Do YYYY");
+    losAngelesTime.innerHTML = losAngelesCurrentTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
   let sydney = document.querySelector("#sydney");
-  let sydneyDate = sydney.querySelector(".date");
-  let sydneyTime = sydney.querySelector(".time");
-  let sydneyCurrentTime = moment().tz("Australia/Sydney");
-  sydneyDate.innerHTML = sydneyCurrentTime.format("MMMM Do YYYY");
-  sydneyTime.innerHTML = sydneyCurrentTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+  if (sydney) {
+    // Sydney
+
+    let sydneyDate = sydney.querySelector(".date");
+    let sydneyTime = sydney.querySelector(".time");
+    let sydneyCurrentTime = moment().tz("Australia/Sydney");
+    sydneyDate.innerHTML = sydneyCurrentTime.format("MMMM Do YYYY");
+    sydneyTime.innerHTML = sydneyCurrentTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+// make dropdown work and replace the city when selected
+
+function updateCity(event) {
+  let timeZone = event.target.value;
+  let cityName = timeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(timeZone);
+  let citiesElement = document.querySelector("#cities");
+  // inject new element into the page
+  citiesElement.innerHTML = `
+    <div class="city">
+      <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      </div>
+        <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
+    "A"
+  )}</small>
+        </div>
+     </div>
+  `;
+}
+
+let citiesSelect = document.querySelector("#city-select");
+citiesSelect.addEventListener("change", updateCity);
